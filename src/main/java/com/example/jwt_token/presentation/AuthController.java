cd9129a3d1aee80130c1f311f2d08f3ec0860983
bhandarypratheeksh@gmail.com <putta@3112>
2023-11-19 14:12:55 +0530

    Initial Update

--- src/main/java/com/example/jwt_token/presentation/AuthController.java
+++ src/main/java/com/example/jwt_token/presentation/AuthController.java
@@ -13,6 +13,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
+
 import java.security.SignatureException;
 
 @RestController
--- src/main/java/com/example/jwt_token/security/JwtConfig.java
+++ src/main/java/com/example/jwt_token/security/JwtConfig.java
@@ -3,8 +3,6 @@ package com.example.jwt_token.security;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class JwtConfig {
